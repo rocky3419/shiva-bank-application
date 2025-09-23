843f1e1b1a29b8a03ed94390122c17dbc363ff12
rocky3419 <rockylocalboy@gmail.com>
2025-09-23 18:46:47 +0530

    Enable CORS for API controller

--- src/main/java/bankingwebsite/UserController.java
+++ src/main/java/bankingwebsite/UserController.java
@@ -2,16 +2,17 @@ package bankingwebsite;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*") // ✅ ADD THIS ANNOTATION
 @RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    // ... your other methods like login and register ...
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
